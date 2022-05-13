53db15918d7949a1fd893b18508d886a39398ee4
Amol Suryawanshi <Amol.Suryawanshi@wolterskluwer.com>
2022-05-13 16:30:05 +0530

    Pizza ordering system

---
+++ PizaaOrderingSystem/PizaaOrderingSystem/PizaaMaker.cs
@@ -0,0 +1,29 @@
+﻿using PizaaOrderingSystem.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizaaOrderingSystem
+{
+    public class PizaaMaker : Pizza
+    {
+        protected Pizza _pizza;
+
+        public PizaaMaker(Pizza pizza)
+        {
+            _pizza = pizza;
+        }
+
+        public override string GetPizaaDetails()
+        {
+            return _pizza.Description;
+        }
+
+        public override double CalculateCost()
+        {
+            return _pizza.CalculateCost();
+        }
+    }  
+}
